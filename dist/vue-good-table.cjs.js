a10815b2b9e41f6bedb0090162fe38ce8fd57aea
Tim White <tim@theaccountancycloud.com>
2020-07-13 14:18:48 +0100

    I'm done

--- dist/vue-good-table.cjs.js
+++ dist/vue-good-table.cjs.js
@@ -8232,7 +8232,7 @@ var __vue_render__$1 = function __vue_render__() {
     staticClass: "footer__navigation vgt-pull-right"
   }, [_typeof(_vm.totalUnreconciled) !== undefined ? _c('span', {
     staticClass: "unreconciled-count"
-  }, [_vm._v("\n      " + _vm._s(_vm.totalUnreconciled) + " uncategorised\n    ")]) : _vm._e(), _vm._v(" "), _c('a', {
+  }, [_vm._v("\n      " + _vm._s(_vm.totalUnreconciled) + " unreconciled\n    ")]) : _vm._e(), _vm._v(" "), _c('a', {
     staticClass: "footer__navigation__page-btn",
     "class": {
       disabled: !_vm.prevIsPossible
--- dist/vue-good-table.js
+++ dist/vue-good-table.js
@@ -8234,7 +8234,7 @@
       staticClass: "footer__navigation vgt-pull-right"
     }, [_typeof(_vm.totalUnreconciled) !== undefined ? _c('span', {
       staticClass: "unreconciled-count"
-    }, [_vm._v("\n      " + _vm._s(_vm.totalUnreconciled) + " uncategorised\n    ")]) : _vm._e(), _vm._v(" "), _c('a', {
+    }, [_vm._v("\n      " + _vm._s(_vm.totalUnreconciled) + " unreconciled\n    ")]) : _vm._e(), _vm._v(" "), _c('a', {
       staticClass: "footer__navigation__page-btn",
       "class": {
         disabled: !_vm.prevIsPossible
